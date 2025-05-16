a9d0478ef44169a165be9f7ba41223b2a6a6db62
MOHAMMAD EMRAN AHMED EMON <ahmed777emran@gmail.com>
2025-05-17 04:10:19 +0600

    JUnit added

---
+++ src/JUnit/Basics/CalculatorTest.java
@@ -0,0 +1,31 @@
+package JUnit.Basics;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    Calculator c= new Calculator();
+
+    @Test
+    public void add() {
+        assertEquals(10,c.add(4,6));
+    }
+
+    @Test
+    public void subtract() {
+        assertEquals(2,c.subtract(6,4));
+    }
+
+    @Test
+    public void multiply() {
+        assertEquals(27,c.multiply(9,3));
+        assertEquals(0,c.multiply(0,5));
+    }
+
+    @Test
+    public void divide() {
+        assertEquals(2,c.divide(28,14));
+        assertEquals(0,c.divide(0,7));
+    }
+}
